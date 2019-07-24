6fd35563a180b0f7c67c50a648a77e666cad5794
injashkin <injashkin@gmail.com>
2019-07-25 00:45:42 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -49,6 +49,7 @@ class App extends React.Component {
               </header>
 
               <Menu
+                input={this.state.input}
                 icon={classes.icon}
                 onClick={this.handleWindowsPosition}
                 windowsLeftRight={this.state.windowsLeftRight}
@@ -83,24 +84,8 @@ class App extends React.Component {
 
           </div>
         </div >
-        <footer className="footer">
-          <div>
-            <i 
-              className="fab fa-free-code-camp" 
-              title="freeCodeCamp"
-            ></i>    
-            injashkin
-          </div>
-          <div className="madeOn">
-            Сделано на:              
-            <i className="fab fa-html5" title="HTML5"> </i>
-            <i className="fab fa-css3" title="CSS3"> </i>            
-            <i className="fab fa-font-awesome" title="Font Awesome"> </i>
-            <i className="fab fa-sass" title="SCSS"> </i>
-            <i className="fab fa-js" title="Javascript"></i>
-            <i className="fab fa-react" title="React"></i>
-          </div>
-        </footer>
+        <Footer />
+
       </div >
     );
   }
@@ -113,12 +98,19 @@ const Menu = (props) => {
     "Расположить окна слева и справа";
   return (
     <div className="memu">
+      <ClearEdit input={props.input} />
       <i
         title={title}
         onClick={props.onClick}
         className={props.icon}
       />
     </div>
+  )
+}
+
+const ClearEdit = (props) => {  
+  return (
+    <i class="fas fa-eraser" title="Очистить редактор для ввода нового текста"></i>
   )
 }
 
@@ -143,10 +135,65 @@ const Previewer = (props) => {
   )
 }
 
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div>
+        <i
+          className="fab fa-free-code-camp"
+          title="freeCodeCamp"
+        ></i>
+        injashkin
+      </div>
+      <div className="madeOn">
+        Сделано на:
+            <i className="fab fa-html5" title="HTML5"> </i>
+        <i className="fab fa-css3" title="CSS3"> </i>
+        <i className="fab fa-font-awesome" title="Font Awesome"> </i>
+        <i className="fab fa-sass" title="SCSS"> </i>
+        <i className="fab fa-js" title="Javascript"></i>
+        <i className="fab fa-react" title="React"></i>
+      </div>
+    </footer>
+  )
+}
+
 export default App;
 
 const markdownText =
-  `# Добро пожаловать в мой Markdown Previewer на React!
+  `Краткая справка по Markdown
+===============================
+Эта справка представляет собой мой вольный перевод краткого руководства, которое представлено на сайте https://marked.js.org/demo с примерами синтаксиса, который поддерживает [Markdown]. 
+
+[Markdown]: http://daringfireball.net/projects/markdown/
+
+Простое Форматирование Текста
+=============================
+
+Вы можете использовать *звездочки* или _подчеркивания_ для выделения курсивом.  **Двойные звездочки** и __двойное подчеркивание__ для жирного текста.  ***Три звездочки*** или ___три подчеркивания___ для жирного курсива.
+
+Пункты тоже непросто.  Просто есть пустая строка между кусками текста.
+
+> Этот кусок текста находится в блочной кавычке.  Его несколько строк будут
+> отступ немного от остальной части текста.
+>
+> > Несколько уровней блочных котировок также работают.
+
+Иногда вы хотите включить некоторый код, например, когда вы объясняете, как работают HTML-теги \`<h1>\`, или, возможно, вы программист, и вы обсуждаете " someMethod ()".
+
+Если вы хотите включить какой-то код и
+новые строки сохраняются, отступ строки с вкладкой
+или, по крайней мере, четыре места.
+    Дополнительные места работают и здесь.
+Это также называется предварительно отформатированный текст, и это полезно для показа примеров.
+Текст останется как текст, поэтому любой * markdown* или < u>HTML< / u>, который вы добавляете, будет
+не отформатирован.  Таким образом, вы можете показать примеры уценки в a
+уценка документа.
+
+> Вы также можете использовать предварительно отформатированный текст с вашей цитаты
+> пока вы добавляете не менее пяти пробелов.
+
+  # Добро пожаловать в мой Markdown Previewer на React!
 
 ## Это подзаголовок...
 ### А вот некоторые другие интересные вещи:
@@ -168,22 +215,70 @@ function anotherExample(firstLine, lastLine) {
 Или ** _и тем, и другим_**
 И не стесняйтесь ~~зачеркивать ненужное~~.
 
-There's also [links](https://www.freecodecamp.com), and
-> Block Quotes!
+Ссылки
+=====
 
-And if you want to get really crazy, even tables:
+Давайте перейдем по ссылке на несколько сайтов.
+Во-первых, давайте использовать базовый URL, например 
+<http://www.github.com>. отлично подходит для текста, 
+но некрасиво для HTML.
+Далее идет встроенная ссылка на [Google](http://www.google.com). немного приятнее.
+Это ссылка эталонный стиль на [Wikipedia] [1].
+Наконец, вот привлекательная ссылка на [Yahoo].  
+Эталонный стиль и привлекательная ссылка  автоматически 
+используют ссылки, определенные ниже, но они могут быть 
+определены *в любом месте* в markdown и удалены из HTML.  
+Имена  
+также нечувствительны   
+к регистру,<br> поэтому вы можете  
+использовать [YaHoO] и правильно связать его.
 
-Wild Header | Crazy Header | Another Header?
------------- | ------------- | ------------- 
-Your content can | be here, and it | can be here....
-And here. | Okay. | I think we get it.
+[1]: http://www.wikipedia.org/
+[Yahoo]: http://www.yahoo.com/
 
-- And of course there are lists.
-  - Some are bulleted.
-     - With different indentation levels.
-        - That look like this.
+Атрибуты заголовка могут быть добавлены к ссылкам путем 
+добавления текста после ссылки.
+Это [онлайн ссылка](http://www.bing.com "Bing") с названием 
+"Bing".
+Вы также можете перейти к [W3C][2] и, возможно, посетить [друг]а.
+
+[2]: http://w3c.org (W3C выпускает спецификации для веб-вещей)
+[Друг]: http://facebook.com/ "Facebook!"
+
+#### Как открывать ссылки в новом окне
+
+Все ссылки в стандартном редакторе Markdown открываются в 
+этом же окне. Только HTML-разметка позволяет изменить это. 
+Никакие другие способы не будут работать стабильно и всегда, 
+и в этом есть некоторое неудобство. 
+Пример <a href="https://google.ru" target="_blank" title="Гугл">c HTML-разметкой</a>, а также
+<a href="http://jinv.ru" target="_blank">без разметки</a>
+
+Адреса электронной почты в обычном тексте не связаны: test@example.com.
+Адреса электронной почты, заключенные в угловые скобки, связаны: <test@example.com>.
+Они также запутаны, так что электронная почта сбора спама роботов, надеюсь, не получит их.
+
+> Это цитата!
+И она будет продолжатся, пока не нажмете два раза "Enter"
+
+И если вы хотите действительно сойти с ума, даже таблицы:
+
+Заголовок | Заголовок | Еще Заголовок?
+------------| --------------| --------------
+Ваш контент может | быть здесь, и он | может быть здесь....
+И здесь. | Окей. | - Я думаю, мы поняли.
+
+- И конечно же есть списки.
+  - Некоторые маркированы.
+     - С различными уровнями отступа.
+        - Это выглядит вот так.
 
 
+1. И есть нумерованные списки тоже.
+1. Используйте только 1s, если вы хотите! 
+1. Но список можно продолжать...
+- Даже если вы используете тире или звездочки.
+* И последнее, но не менее важное: давайте не будем забывать встроенные изображения:
 1. And there are numbererd lists too.
 1. Use just 1s if you want! 
 1. But the list goes on...
